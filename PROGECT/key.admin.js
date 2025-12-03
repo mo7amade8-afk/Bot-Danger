a7140d568a012a8d9a129c637fecfab9a26f8223
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-04 00:33:01 +0100

    Update key.admin.js

--- PROGECT/key.admin.js
+++ PROGECT/key.admin.js
@@ -1,1 +1,8 @@
+import adminControl from "../ADMIN/admin_control.js";
 
+export default function key_admin(ctx) {
+    const text = ctx.message.text;
+
+    // ارسال الطلب الى admin_control
+    adminControl(ctx, text);
+}
