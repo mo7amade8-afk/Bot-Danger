b148e5e31d6042af1ed86343975f27c71c693fb4
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-03 22:16:06 +0100

    Delete BOT TILIGRAM

--- PROGECKT/BOT TILIGRAM/key_admin.js
+++
@@ -1,8 +0,0 @@
-import adminControl from "../ADMIN/admin_control.js";
-
-export default function key_admin(ctx) {
-    const text = ctx.message.text;
-
-    // ارسال الطلب الى admin_control
-    adminControl(ctx, text);
-}
